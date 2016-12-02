bba7ab1a0aeceed85b4bc88d8acf67e24a6d7b53
joakim <joakim.karl.larsson@gmail.com>
2016-12-02 14:45:18 +0100

    added dank memes to try sessions

---
+++ memes.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>MEJMEJ</title>
+<meta charset="UTF-8">
+</head>
+<body>
+<img src="http://cdn.thedailybeast.com/content/dailybeast/articles/2016/05/26/how-pepe-the-frog-became-a-nazi-trump-supporter-and-alt-right-symbol/jcr:content/image.crop.800.500.jpg/48807968.cached.jpg" alt="Mountain View" style="width:304px;height:228px;">
+
+<!--The button-->
+<div class="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="false" data-auto-logout-link="true"></div>
+
+</body>
+</html>
