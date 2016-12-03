ab68ab40313bb45dec2ca38b8bbb21c85a8b956e
leffemedkniven <leffemedkniven@gmail.com>
2016-12-03 14:57:13 +0100

    API change

--- api.php
+++ api.php
@@ -21,6 +21,14 @@
 			else if(!empty($_GET["restaurants"])){
 				get_restaurants();
 			}
+			else if(!empty($_GET["review_ID"])){
+				$review_ID=intval($_GET["review_ID"]);
+				get_reviews($review_ID);
+			}
+			else if(!empty($_GET["review_ID"])){
+				get_reviews();
+			}
+
 			else {
 				header("HTTP/1.0 405 Method Not Allowed");
 				break;
@@ -73,9 +81,31 @@
 		{
 			$response[]=$row;
 		}
+
 		header('Content-Type: application/json');
 		echo json_encode($response);
+  }
+
+	function get_reviews($restaurant_ID=0)
+	{
+		global $connection;
+		$query=$connection->prepare("SELECT * FROM reviews");
+
+		if($review_ID != 0)
+		{
+			$query=$connection->prepare("SELECT * FROM reviews WHERE review_ID=:id");
+			$query->bindParam(':id',$review_ID);
 		}
+		$response=array();
+		$query->execute();
+		while($row=$query->fetch(PDO::FETCH_ASSOC))
+		{
+			$response[]=$row;
+		}
+
+		header('Content-Type: application/json');
+		echo json_encode($response);
+	}
 
 	function insert_restaurant()
 	{
@@ -85,69 +115,62 @@
 		$description=$_POST['description'];
 		$location=$_POST['location'];
 
-			$query=$connection->prepare('INSERT INTO restaurants(name, picture, description, location) VALUES (:name, :picture, :description, :location)');
-			$query->bindParam(':name',$name);
-			$query->bindParam(':picture',$picture);
-			$query->bindParam(':description',$description);
-			$query->bindParam(':location',$location);
+		$query=$connection->prepare('INSERT INTO restaurants(name, picture, description, location) VALUES (:name, :picture, :description, :location)');
+		$query->bindParam(':name',$name);
+		$query->bindParam(':picture',$picture);
+		$query->bindParam(':description',$description);
+		$query->bindParam(':location',$location);
 
-    	if($query->execute())
-    	{
-      	$response=array('status' => 1, 'info' =>'Restaurant added.');
-    	}
-			else
-	 		{
-		 		$response=array('status' => 0, 'info' =>'Addition failed, please try again.');
-	 		}
+    if($query->execute()){
+    	$response=array('status' => 1, 'info' =>'Restaurant added.');
+    }
+		else{
+			$response=array('status' => 0, 'info' =>'Addition failed, please try again.');
+		}
 
 	 	header('Content-Type: application/json');
 	 	echo json_encode($response);
+	}
+
+	function insert_review()
+	{
+		global $connection;
+		$user_ID=$_POST['user_ID'];
+		$restaurant_ID=$_POST['restaurant_ID'];
+		$review=$_POST['review'];
+		$rating=$_POST['rating'];
+
+		$query=$connection->prepare('INSERT INTO restaurants(name, picture, description, location) VALUES (:user_ID, :restaurant_ID, :review, :rating)');
+		$query->bindParam(':user_ID',$user_ID);
+		$query->bindParam(':restaurant_ID',$restaurant_ID);
+		$query->bindParam(':review',$review);
+		$query->bindParam(':rating',$rating);
+
+	  if($query->execute()){
+	    $response=array('status' => 1, 'info' =>'Review added.');
+	  }
+		else{
+			$response=array('status' => 0, 'info' =>'Addition failed, please try again.');
 		}
 
-		function insert_review()
-		{
-			global $connection;
-			$user_ID=$_POST['user_ID'];
-			$restaurant_ID=$_POST['restaurant_ID'];
-			$review=$_POST['review'];
-			$rating=$_POST['rating'];
+		header('Content-Type: application/json');
+		echo json_encode($response);
+	}
 
-				$query=$connection->prepare('INSERT INTO restaurants(name, picture, description, location) VALUES (:user_ID, :restaurant_ID, :review, :rating)');
-				$query->bindParam(':user_ID',$user_ID);
-				$query->bindParam(':restaurant_ID',$restaurant_ID);
-				$query->bindParam(':review',$review);
-				$query->bindParam(':rating',$rating);
+	function delete_restaurant($restaurant_ID)
+	{
+		global $connection;
 
-	    	if($query->execute())
-	    	{
-	      	$response=array('status' => 1, 'info' =>'Review added.');
-	    	}
-				else
-		 		{
-			 		$response=array('status' => 0, 'info' =>'Addition failed, please try again.');
-		 		}
+		$query=$connection->prepare('DELETE FROM restaurants WHERE restaurant_ID=:id');
+		$query->bindParam(':id',$restaurant_ID);
 
-		 	header('Content-Type: application/json');
-		 	echo json_encode($response);
-			}
-
-		function delete_restaurant($restaurant_ID)
-		{
-				global $connection;
-
-				$query=$connection->prepare('DELETE FROM restaurants WHERE restaurant_ID=:id');
-				$query->bindParam(':id',$restaurant_ID);
-
-				if($query->execute())
-				{
-					$response=array('status' => 1, 'info' =>'Restaurant deleted.');
-				}
-				else
-				{
+		if($query->execute()){
+			$response=array('status' => 1, 'info' =>'Restaurant deleted.');
+		}
+		else{
 					$response=array('status' => 0, 'info' =>'Deletion failed, please try again.');
-				}
-
-
-			header('Content-Type: application/json');
-			echo json_encode($response);
 		}
+
+		header('Content-Type: application/json');
+		echo json_encode($response);
+	}
