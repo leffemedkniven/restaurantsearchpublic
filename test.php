7f8f74f5b822dde94f9d3c4bf5f16072b8b907c9
leffemedkniven <leffemedkniven@gmail.com>
2016-12-06 10:36:41 +0100

    Testing picture upload

--- test.php
+++ test.php
@@ -16,28 +16,28 @@ $type=$_POST['type'];
 
 $public_urls = [];
 foreach($_FILES[$userfile][$name] as $idx => $name) {
-  if ($_FILES[$userfile][$type][$idx] === 'image/jpeg' || $_FILES[$userfile][$type][$idx] === 'image/png') {
+  // if ($_FILES[$userfile][$type][$idx] === 'image/jpeg' || $_FILES[$userfile][$type][$idx] === 'image/png') {
+  //
+  //   $original = $root_path . $name;
+  //   echo '<pre>';
+  //   if(move_uploaded_file($_FILES['userfile']['tmp_name'][$idx], $original)){
+  //     echo "File is valid, and was successfully uploaded.\n";
+  //     $response=array('status' => 1, 'info' =>'Image uploaded.');
+  //   }
+  //   else {
+  //   echo "Possible file upload attack!\n";
+  //   $response=array('status' => 1, 'info' =>'Image not uploaded.');
+  //   }
+  //
+  //   echo 'Here is some more debugging info:';
+  //   print_r($_FILES);
+  //
+  //   print "</pre>";
+  // } else {
+  //   echo "Not a jpeg/png\n";
+  // }
 
-    $original = $root_path . $name;
-    echo '<pre>';
-    if(move_uploaded_file($_FILES['userfile']['tmp_name'][$idx], $original)){
-      echo "File is valid, and was successfully uploaded.\n";
-    //  $response=array('status' => 1, 'info' =>'Image uploaded.');
-    }
-    else {
-    echo "Possible file upload attack!\n";
-  //  $response=array('status' => 1, 'info' =>'Image not uploaded.');
-    }
-
-    echo 'Here is some more debugging info:';
-    print_r($_FILES);
-
-    print "</pre>";
-  } else {
-    echo "Not a jpeg/png\n";
-  }
-
-//  header('Content-Type: application/json');
-  //echo json_encode($response);
+  header('Content-Type: application/json');
+  echo json_encode($response);
 
 }
