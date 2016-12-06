f8a9847b4ea4ba024138e18ec027e8102e8d6281
leffemedkniven <leffemedkniven@gmail.com>
2016-12-06 16:13:12 +0100

    Testing picture upload

--- direct_upload.php
+++ direct_upload.php
@@ -15,22 +15,22 @@ if (strncmp("5.5", phpversion(), strlen("5.5")) != 0) {
 </html>
 
 <?php
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
-
-$tmpfile = $_FILES['image']['tmp_name'];
-$filename = basename($_FILES['image']['name']);
-$data = array(
-  'uploaded_file' => curl_file_create($tmpfile, $_FILES['image']['type'], $filename
-);
-
-
-  $url = 'https://whatsdown-d627f.appspot.com/api/?uploadImage=1';
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_POST, true);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  $response_json = curl_exec($ch);
-  curl_close($ch);
-  $response=json_decode($response_json, true);
-}
+// if($_SERVER['REQUEST_METHOD'] === 'POST'){
+//
+// $tmpfile = $_FILES['image']['tmp_name'];
+// $filename = basename($_FILES['image']['name']);
+// $data = array(
+//   'uploaded_file' => curl_file_create($tmpfile, $_FILES['image']['type'], $filename
+// );
+//
+//
+//   $url = 'https://whatsdown-d627f.appspot.com/api/?uploadImage=1';
+//   $ch = curl_init($url);
+//   curl_setopt($ch, CURLOPT_POST, true);
+//   curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+//   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//   $response_json = curl_exec($ch);
+//   curl_close($ch);
+//   $response=json_decode($response_json, true);
+// }
 ?>
