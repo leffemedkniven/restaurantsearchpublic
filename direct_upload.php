a34af7c42f2bd0cde22fb404fc699085e525ae11
joakim <joakim.karl.larsson@gmail.com>
2016-12-06 16:13:57 +0100

    restaurant

--- direct_upload.php
+++ direct_upload.php
@@ -6,10 +6,31 @@ if (strncmp("5.5", phpversion(), strlen("5.5")) != 0) {
 ?>
 <html>
 <body>
-<form action="handle_upload" method="post" enctype="multipart/form-data">
+<form action="upload" method="POST" enctype="multipart/form-data">
   Send these files:<p/>
   <input name="userfile[]" type="file" multiple="multiple"/><p/>
   <input type="submit" value="Send files" />
 </form>
 </body>
 </html>
+
+<?php
+// if($_SERVER['REQUEST_METHOD'] === 'POST'){
+//
+// $tmpfile = $_FILES['image']['tmp_name'];
+// $filename = basename($_FILES['image']['name']);
+// $data = array(
+//   'uploaded_file' => curl_file_create($tmpfile, $_FILES['image']['type'], $filename
+// );
+//
+//
+//   $url = 'https://whatsdown-d627f.appspot.com/api/?uploadImage=1';
+//   $ch = curl_init($url);
+//   curl_setopt($ch, CURLOPT_POST, true);
+//   curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+//   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//   $response_json = curl_exec($ch);
+//   curl_close($ch);
+//   $response=json_decode($response_json, true);
+// }
+?>
