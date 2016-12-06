b82e210abdf483f07f61fa804e94556ae14ac9d2
joakim <joakim.karl.larsson@gmail.com>
2016-12-06 17:45:35 +0100

    restaurant

--- pages/restaurant.php
+++ pages/restaurant.php
@@ -17,6 +17,9 @@
 		echo("<p>".$row['description']."</p>");
 		echo("<p>".$row['location']."</p>");
 		echo("<p>".$row['picture']."</p>");
+		$rest_name = $row['name'];
+		$rest_desc = $row['description'];
+		$rest_loc = $row['location'];
 	}
 
 ?>	
@@ -69,11 +72,10 @@
                     <div class="caption-full">
                         <h4 class="pull-right">$24.99</h4>
                         <h4><?php echo $rest_name; ?></h4>
-                        <p>See more snippets like these online store reviews at <a target="_blank" href="http://bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
-                        <p>Want to make these reviews work? Check out
-                            <strong><a href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">this building a review system tutorial</a>
-                            </strong>over at maxoffsky.com!</p>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                        <p><?php echo $rest_desc; ?>
+			</p>
+                        <p><?php echo $rest_loc; ?>
+			</p>
                     </div>
                     <div class="ratings">
                         <p class="pull-right">n reviews</p>
