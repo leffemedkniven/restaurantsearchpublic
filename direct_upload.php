42c6c1c3bbcbad6903654355e81a1bf729a43d2c
leffemedkniven <leffemedkniven@gmail.com>
2016-12-07 09:49:28 +0100

    Testing picture upload

--- direct_upload.php
+++ direct_upload.php
@@ -19,9 +19,9 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
 $tmpfile = $_FILES['image']['tmp_name'];
 $filename = basename($_FILES['image']['name']);
-// $data = array(
-//   'uploaded_file' => curl_file_create($tmpfile, $_FILES['image']['type'], $filename
-// );
+$data = array(
+  'uploaded_file' => curl_file_create($tmpfile, $_FILES['image']['type'], $filename
+);
 //
 //
 //   $url = 'https://whatsdown-d627f.appspot.com/api/?uploadImage=1';
