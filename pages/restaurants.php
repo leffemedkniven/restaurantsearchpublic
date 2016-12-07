a376e6bca4d96eaf2824e19afd909346fac1dd7d
leffemedkniven <leffemedkniven@gmail.com>
2016-12-07 16:07:59 +0100

    Testing picture upload

--- pages/restaurants.php
+++ pages/restaurants.php
@@ -65,6 +65,7 @@
 			echo("<p>".$row['description']."</p>");
 			echo("<p>".$row['location']."</p>");
 			echo("<p>".$row['picture']."</p>");
+      echo("<img src=".$row['picture']".">");
         		echo("<p><a class=\"btn btn-default\" id=".$row[name]." onclick=\"myFunction(this.id)\" role=\"button\">View details </a></p>");
 			echo("</div>");
 	    	}
