1d96b410617a01e6bf53acce5975386d44f682df
leffemedkniven <leffemedkniven@gmail.com>
2016-12-07 19:09:57 +0100

    Testing picture upload

--- direct_upload.php
+++ direct_upload.php
@@ -4,6 +4,9 @@ if (strncmp("5.5", phpversion(), strlen("5.5")) != 0) {
   die("Direct uploads require the PHP 5.5 runtime. Your runtime: " . phpversion());
 }
 ?>
+<head>
+<script src="jquery-3.1.1.min.js"></script>
+</head>
 <html>
 <body>
 <form id="database" method="POST" enctype="multipart/form-data">
