84164cf85ed7b3af0f2ee769c43247118d72fe06
joakim <joakim.karl.larsson@gmail.com>
2016-12-07 17:56:44 +0100

    resturant

--- pages/restaurant.php
+++ pages/restaurant.php
@@ -100,13 +100,13 @@
 				var uID = document.getElementById('user_ID').value;
 				var rID = document.getElementById('rest_ID').value;
 				var rate = document.getElementById('rating').value;
-				alert(rev+":"+uID+":"+rID+":"+rate);
-				$.ajax({
-			url: 'https://whatsdown-d627f.appspot.com/api/?restaurantReviews=1',
-				    type: 'post',
-			data: {"user_ID": uID, "restaurant_ID": rID, "rating": rate, "review": rev},
-				    success: function(response) { console.log(response); }
-				});
+				alert(rev);
+			//	$.ajax({
+			//url: 'https://whatsdown-d627f.appspot.com/api/?restaurantReviews=1',
+			//	    type: 'post',
+			//data: {"user_ID": uID, "restaurant_ID": rID, "rating": rate, "review": rev},
+			//	    success: function(response) { console.log(response); }
+			//	});
 			}
 		</script>
 
