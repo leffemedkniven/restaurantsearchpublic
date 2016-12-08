784844eaa9d77aea585aafe13aaf9e0b34d13cad
leffemedkniven <leffemedkniven@gmail.com>
2016-12-08 14:45:20 +0100

    Testing picture upload

--- pages/review.php
+++ pages/review.php
@@ -14,22 +14,22 @@ $rate=$_POST['rate'];
 	$url = 'https://whatsdown-d627f.appspot.com/api/?insertReview=1';
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_HTTPPOST, true);
-	echo "hej";
+	// echo "hej";
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-	echo "svej";
+	// echo "svej";
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	echo "hallon";
+	// echo "hallon";
 	$response_json = curl_exec($ch);
-	echo "paj";
+	// echo "paj";
 	curl_close($ch);
-	echo "lol";
+	// echo "lol";
 	$response=json_decode($response_json, true);
-	echo "slut";
-    if ($response['status']==1){
-        header('Location: https://whatsdown-d627f.appspot.com/restaurant/?id='.$restaurant_ID.);
-        exit;
-    } else {
-	echo "RIP";
-    }
+	// echo "slut";
+  //   if ($response['status']==1){
+  //       header('Location: https://whatsdown-d627f.appspot.com/restaurant/?id='.$restaurant_ID.);
+  //       exit;
+  //   } else {
+	// echo "RIP";
+  //   }
 
 ?>
