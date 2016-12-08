3f155a99733f791d6037fdb9b559b81ac8f23012
leffemedkniven <leffemedkniven@gmail.com>
2016-12-08 11:34:32 +0100

    Testing picture upload

--- direct_upload.php
+++ direct_upload.php
@@ -16,7 +16,7 @@ $("#data").submit(function(e) {
         var formData = new FormData($(this)[0]);
 
         $.ajax({
-            url: "https://whatsdown-d627f.appspot.com/api/?uploadImage=4",
+            url: "https://whatsdown-d627f.appspot.com/api/?uploadImage=15",
             type: "POST",
             data: formData,
             async: false,
