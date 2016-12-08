01c4fc3136fd02a6ce10a15b235e4e2518997b4a
leffemedkniven <leffemedkniven@gmail.com>
2016-12-08 17:15:48 +0100

    Testing picture upload

--- pages/review.php
+++ pages/review.php
@@ -21,7 +21,7 @@ $rate=$_POST['rate'];
 	$response=json_decode($response_json, true);
 
   //   if ($response['status']==1){
-  header("Location: https://whatsdown-d627f.appspot.com/";
+  header("Location: https://whatsdown-d627f.appspot.com/");
 //  restaurant/?id=".$restaurant_ID
   //     exit;
   //    } else {
