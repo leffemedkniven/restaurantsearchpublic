f4d69d3b32d47d73656225ec4589f90a950d4cd9
leffemedkniven <leffemedkniven@gmail.com>
2016-12-08 16:17:35 +0100

    Testing picture upload

--- pages/review.php
+++ pages/review.php
@@ -21,13 +21,11 @@ $rate=$_POST['rate'];
 	$response=json_decode($response_json, true);
   var_dump($response);
 
-
-	// echo "slut";
-  //   if ($response['status']==1){
-  //       header('Location: https://whatsdown-d627f.appspot.com/restaurant/?id='.$restaurant_ID.);
-  //       exit;
-  //   } else {
-	// echo "RIP";
-  //   }
+    if ($response['status']==1){
+         header('Location: https://whatsdown-d627f.appspot.com/restaurant/?id='.$restaurant_ID.);
+      exit;
+     } else {
+	 echo "Something went wrong";
+    }
 
 ?>
