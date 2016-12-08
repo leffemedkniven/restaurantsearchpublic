4c4e77858998940cdb7d49ca092a27ff19e7035d
joakim <joakim.karl.larsson@gmail.com>
2016-12-08 10:04:34 +0100

    resturant

--- pages/restaurant.php
+++ pages/restaurant.php
@@ -86,9 +86,9 @@
                                         <div class="message-text">
                                             <textarea class="no-resize-bar form-control" id="message" rows="2" placeholder="Write a review"></textarea>
 				<form>
-				<!--<input type="hidden" id="rest_ID" value="<?php echo $rest_ID; ?>" />
+				<input type="hidden" id="rest_ID" value="<?php echo $rest_ID; ?>" />
 				<input type="hidden" id="user_ID" value="<?php echo $user_ID; ?>" />
-                  -->              </form>     </div>
+                                </form>     </div>
                                     </div>
                                 </div>
                     <div class="text-right">
