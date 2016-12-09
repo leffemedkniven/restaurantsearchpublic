8836d152bbefe071cc38ea359612664e4156e8b1
joakim <joakim.karl.larsson@gmail.com>
2016-12-09 17:30:34 +0100

    admin start

--- login.php
+++ login.php
@@ -1,10 +1,9 @@
 <?php
-<<<<<<< HEAD
+
 session_start();
-=======
+
 $x = json_decode($_POST["data"]);
 $y = json_decode($_POST["data"]);
->>>>>>> ce9790c396c0fb5e1c2ed8ecca35cc863280fedc
 
 $user_name = json_decode($_POST['name']);
 $user_id = json_decode($_POST['id']);
@@ -16,11 +15,10 @@ $_SESSION['user_name'] = 'Jokke';
 
 echo $user_name;
 echo ":Tjena:";
-<<<<<<< HEAD
+
 echo $user_id;
-=======
+
 print_r("hih".$y);
 
->>>>>>> ce9790c396c0fb5e1c2ed8ecca35cc863280fedc
 
 ?>
