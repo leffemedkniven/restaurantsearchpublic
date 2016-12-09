23a2cbc0fd008f3a6bec8122adb5a4efc1375a82
leffemedkniven <leffemedkniven@gmail.com>
2016-12-09 15:54:40 +0100

    Fixing login

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title>COWS</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
