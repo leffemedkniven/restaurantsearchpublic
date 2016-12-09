69a6290d5aeb42a85b74e45acb25c8175b8b9f62
joakim <joakim.karl.larsson@gmail.com>
2016-12-09 16:57:09 +0100

    admin start

--- login.php
+++ login.php
@@ -1,11 +1,8 @@
 <?php
 $x = json_decode($_POST['name']);
+$y = json_decode($_POST['id']);
 echo $x;
-
-
-$user_name = $_POST['name'];
-$user_id = $_POST['id'];
-echo $user_name;
-echo $user_id;
 echo ":Tjena:";
+echo $y;
+
 ?>
