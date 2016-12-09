6fd4f1072007ee997fcb0cfb8040d7ccb33abcdd
leffemedkniven <leffemedkniven@gmail.com>
2016-12-09 15:50:44 +0100

    Fixing login

--- index.php
+++ index.php
@@ -81,6 +81,10 @@
 		  type: "POST",
 		  url: 'https://whatsdown-d627f.appspot.com/login.php',
 		  data: {'name':response.name},
+      error: function (request, error) {
+        console.log(arguments);
+        alert(" Can't do because: " + error);
+      },
 		  success: function(data,status){
                 	alert("Data" + data +"status"+status);
 		              }
