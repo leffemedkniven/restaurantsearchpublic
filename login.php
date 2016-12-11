d52e195373cb4c42cd74a86795a2fca808bbf1bd
leffemedkniven <leffemedkniven@gmail.com>
2016-12-11 18:37:19 +0100

    Fixing login

--- login.php
+++ login.php
@@ -2,11 +2,11 @@
 
 session_start();
 
-// $x = json_decode($_POST['name']);
-$y = json_decode($_POST['data']);
-$data = $_POST['data'];
-// $user_name = json_decode($_POST['name']);
-// $user_id = json_decode($_POST['id']);
+$x = json_decode($_POST['name']);
+$y = json_decode($_POST["data"]);
+
+$user_name = json_decode($_POST['name']);
+$user_id = json_decode($_POST['id']);
 
 
 $_SESSION['user_id'] = "1";
@@ -14,15 +14,12 @@ $_SESSION['user_name'] = 'Jokke';
 $_SESSION['admin'] = true;
 
 
-// echo $user_name;
-// echo ":Tjena:";
-//
-// echo $user_id;
-// echo $x;
+echo $user_name;
+echo ":Tjena:";
 
-print_r("hih2".$data);
-echo $data;
-$fb = $facebook->getUser();
-echo $fb;
-print_r("hih".$fb);
+echo $user_id;
+echo $x;
+print_r("hih".$y);
+
+
 ?>
