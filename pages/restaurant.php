2b5a5cb2d67335b4d3741ae28cdaef43776f863b
leffemedkniven <leffemedkniven@gmail.com>
2016-12-11 21:57:54 +0100

    Fixing login

--- pages/restaurant.php
+++ pages/restaurant.php
@@ -8,8 +8,8 @@ if($_SESSION['user_id']===""){
 }
 
 $fb = new Facebook\Facebook([
-  'app_id' => '{app-id}',
-  'app_secret' => '{app-secret}',
+  'app_id' => '1814790452137377', // Replace {app-id} with your app id
+  'app_secret' => '006b213f54e5c9d124167fdde6e8d29a',
   'default_graph_version' => 'v2.2',
   ]);
 
