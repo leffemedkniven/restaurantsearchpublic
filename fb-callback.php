fa38d7a2167774fa519a7d6341dfd8c2a5194b66
leffemedkniven <leffemedkniven@gmail.com>
2016-12-11 19:33:34 +0100

    Fixing login

--- fb-callback.php
+++ fb-callback.php
@@ -1,5 +1,5 @@
 <?php
-<?php require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 $fb = new Facebook\Facebook([
   'app_id' => '344026762636411', // Replace {app-id} with your app id
