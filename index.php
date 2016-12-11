03c40c63deef19ce1a0c8bc6b963ba569ef79e1e
leffemedkniven <leffemedkniven@gmail.com>
2016-12-11 14:24:25 +0100

    Fixing login

--- index.php
+++ index.php
@@ -80,11 +80,12 @@
 	FB.api('/me', function(response) {
 		//var name = JSON.stringify(response.name);
 		//var id = JSON.stringify(response.id);
+     var test = "dick";
     var stringresponse = JSON.stringify(response);
 		$.ajax({
 		  type: "POST",
 		  url: 'https://whatsdown-d627f.appspot.com/login.php',
-		  data: {'name':'stringresponse'},
+		  data: {'test': test},
       error: function (request, error) {
         ///console.log(arguments);
         alert(" Can't do because: " + error);
