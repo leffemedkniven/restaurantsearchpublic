d077be6c49d3b7f9b511f416a1cd5874173a6a00
leffemedkniven <leffemedkniven@gmail.com>
2016-12-11 20:20:48 +0100

    Fixing login

--- fb-callback.php
+++ fb-callback.php
@@ -72,4 +72,4 @@ $_SESSION['fb_access_token'] = (string) $accessToken;
 
 // User is logged in with a long-lived access token.
 // You can redirect them to a members-only page.
-header("Location: https://whatsdown-d627f.appspot.com/browse/");
+header("Location: https://whatsdown-d627f.appspot.com/");
