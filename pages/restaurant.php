c6e4ca576aae1878f703e82eacc6959d7512976e
leffemedkniven <leffemedkniven@gmail.com>
2016-12-11 12:23:46 +0100

    Fixing login

--- pages/restaurant.php
+++ pages/restaurant.php
@@ -71,7 +71,6 @@ if($_SESSION['user_id']===""){
 		$rest_desc = $row['description'];
 		$rest_loc = $row['location'];
 		$rest_pic = $row['picture'];
-
 	}
 ?>
 
@@ -87,17 +86,14 @@ if($_SESSION['user_id']===""){
                     <div class="imageupload">
                       <center>
                       <form id="data" method="POST" enctype="multipart/form-data">
-                        
+
                         <input type="hidden" name="restaurant_ID" id="restaurant_ID" value="<?php echo $rest_ID; ?>" />
-<<<<<<< HEAD
+
                         <?php
 				if(isset($_SESSION['user_id'])){
-=======
-                        <?php 
 				if($_SESSION['admin']==true){
 					echo("Upload image:<p/>");
                        			echo("<input name=\"file\" type=\"file\" /><p/>");
->>>>>>> faecb3db6f6668ae60ef679d4584c15366c857da
 					echo("<input type=\"submit\" value=\"Upload image\" />");
 				}
 			?>
@@ -105,7 +101,7 @@ if($_SESSION['user_id']===""){
                       </center>
                     </div>
                 </div>
-	
+
                		<div class="well">
 			       <div class="row send-wrap">
                                     <div class="send-message">
@@ -119,7 +115,6 @@ if($_SESSION['user_id']===""){
 			echo("<input type=\"radio\" name=\"rate\" id=\"r3\" value=\"3\" checked> 3");
 			echo("<input type=\"radio\" name=\"rate\" id=\"r4\" value=\"4\" checked> 4");
 			echo("<input type=\"radio\" name=\"rate\" id=\"r5\" value=\"5\" checked> 5");
-
         echo("Date of your restaurant visit:");
         echo("<input type=\"date\" name=\"visitdate\">");
 	echo("<textarea class=\"no-resize-bar form-control\" name=\"review\" id=\"review\" rows=\"2\" placeholder=\"Write a review\"></textarea>");
@@ -128,12 +123,12 @@ if($_SESSION['user_id']===""){
 
 			<input type="hidden" name="restaurant_ID" id="restarant_ID" value="<?php echo $rest_ID; ?>" />
 			<input type="hidden" name="user_ID" id="user_ID" value="<?php echo $user_ID; ?>" />
-                                </form>     
+                                </form>
 					</div>
                                     </div>
                                 </div>
                     <hr>
-	
+
 		    <?php
 			$url = 'https://whatsdown-d627f.appspot.com/api/?restaurantReviews='.$rest_ID;
 			$ch = curl_init($url);
@@ -186,7 +181,6 @@ if($_SESSION['user_id']===""){
     var rest_ID='<?php echo $rest_ID; ?>';
     $("#data").submit(function(e) {
             var formData = new FormData($(this)[0]);
-
             $.ajax({
                 url: "https://whatsdown-d627f.appspot.com/api/?uploadImage="+rest_ID,
                 type: "POST",
@@ -200,7 +194,6 @@ if($_SESSION['user_id']===""){
                 contentType: false,
                 processData: false
             });
-
             e.preventDefault();
         })
     </script>
