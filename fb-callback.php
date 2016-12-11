5e775383e3dcb6b1d3428ea8928eac62e0693691
leffemedkniven <leffemedkniven@gmail.com>
2016-12-11 21:09:36 +0100

    Fixing login

--- fb-callback.php
+++ fb-callback.php
@@ -49,13 +49,13 @@ $oAuth2Client = $fb->getOAuth2Client();
 $tokenMetadata = $oAuth2Client->debugToken($accessToken);
 echo '<h3>Metadata</h3>';
 var_dump($tokenMetadata);
+header("Location: https://whatsdown-d627f.appspot.com/");
 
 // Validation (these will throw FacebookSDKException's when they fail)
 $tokenMetadata->validateAppId(344026762636411); // Replace {app-id} with your app id
 // If you know the user ID this access token belongs to, you can validate it here
 //$tokenMetadata->validateUserId('123');
 $tokenMetadata->validateExpiration();
-header("Location: https://whatsdown-d627f.appspot.com/");
 if (! $accessToken->isLongLived()) {
   // Exchanges a short-lived access token for a long-lived one
   try {
