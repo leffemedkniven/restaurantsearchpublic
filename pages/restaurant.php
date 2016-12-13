892fe9f1288c873fe4713f8b670b78db745c2ed8
leffemedkniven <leffemedkniven@gmail.com>
2016-12-13 11:16:16 +0100

    Testing

--- pages/restaurant.php
+++ pages/restaurant.php
@@ -16,6 +16,7 @@ $fb = new Facebook\Facebook([
   ]);
 
 $at = $_SESSION['fb_access_token'];
+$_SESSION['admin'] = true;
 try {
   // Returns a `Facebook\FacebookResponse` object
   $response = $fb->get('/me?fields=id,name', $at);
@@ -29,7 +30,7 @@ try {
 $user = $response->getGraphUser();
 
 echo 'Name: ' . $user['name'];
-echo 'ID: ' . $user['fbid'];
+
 ?>
 <html lang="en">
 <head>
