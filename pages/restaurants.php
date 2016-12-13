58112e2ca98a84c9e38d5bdadcc6e5c35934fcc6
joakim <joakim.karl.larsson@gmail.com>
2016-12-13 14:09:08 +0100

    logout

--- pages/restaurants.php
+++ pages/restaurants.php
@@ -23,6 +23,7 @@ $fb = new Facebook\Facebook([
 
 $at = $_SESSION['fb_access_token'];
 $_SESSION['admin'] = true;
+echo $_SESSION['user_ID'];
 try {
   // Returns a `Facebook\FacebookResponse` object
   $response = $fb->get('/me?fields=id,name', $at);
