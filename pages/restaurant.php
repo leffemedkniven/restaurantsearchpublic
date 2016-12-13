b99e9f30b9cd73c9d0f00002b34c7f89467bd210
leffemedkniven <leffemedkniven@gmail.com>
2016-12-13 15:35:29 +0100

    Testing

--- pages/restaurant.php
+++ pages/restaurant.php
@@ -16,7 +16,7 @@ $fb = new Facebook\Facebook([
   ]);
 
 $at = $_SESSION['fb_access_token'];
-$_SESSION['admin'] = true;
+
 try {
   // Returns a `Facebook\FacebookResponse` object
   $response = $fb->get('/me?fields=id,name', $at);
