304d240c6b9c53b0fe308c116ba67f22d4984668
leffemedkniven <leffemedkniven@gmail.com>
2017-01-13 10:59:00 +0100

    Redirect

--- logout.php
+++ logout.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
 session_destroy();
-header('Location: https://whatsdown-d627f.appspot.com/');
+$ini = parse_ini_file('configURL.ini');
+header('Location: '. $ini[app_url]);
  ?>
