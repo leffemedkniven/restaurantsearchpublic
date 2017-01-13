e93db66fbf6c83815f32b8046521bf033eaebd4c
leffemedkniven <leffemedkniven@gmail.com>
2017-01-13 09:59:07 +0100

    Redirect

--- index.php
+++ index.php
@@ -52,6 +52,8 @@
     </div> <!-- /container -->
 
     <?php
+    $ini = parse_ini_file('configURL.ini');
+
     //Establishing facebook app connection
       $fb = new Facebook\Facebook([
           'app_id' => '1814790452137377', // Replace {app-id} with your app id
@@ -61,7 +63,7 @@
     //Login
       $helper = $fb->getRedirectLoginHelper();
       $permissions = ['email']; // Optional permissions
-      $loginUrl = $helper->getLoginUrl('https://whatsdown-d627f.appspot.com/fb-callback.php', $permissions);
+      $loginUrl = $helper->getLoginUrl($ini['app_url'] . '/fb-callback.php', $permissions);
       echo '<h2><a href="' . htmlspecialchars($loginUrl) . '"><center>Log in with Facebook!</center></a></h2>';
 
       ?>
